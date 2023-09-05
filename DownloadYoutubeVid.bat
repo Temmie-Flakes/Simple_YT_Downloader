c3fdad19017eccd0bcf9659c5be9488796c2cac8
Temmie-Flakes <gavenbrewster@gmail.com>
2023-09-05 03:13:58 -0400

    Lota stuff
    changed to download latest yt-dlp
    now you can just paste link and it will download the same way as last time.
    fixed alot of file path bugs
    changed which version of it downloads ffmpeg which is half the size

--- DownloadYoutubeVid.bat
+++ DownloadYoutubeVid.bat
@@ -1,47 +1,87 @@
 @echo off
-REM Ask the user to choose between downloading video or audio
-echo What would you like to do:&echo\1)downloads video&echo\2)downloads audio only&echo\
-set /p choice=">>"
+REM Ask user for what they want to do
+REM pasteing youtube link will use last setting or if first time, just video.
+REM 1=download
+echo What would you like to do:&echo\1)download video&echo\2)download audio only&echo\Paste URL)download using last settings&echo\
+set /p videochoice=">>"
 
-REM Ask the user for the path of the video
-set /p videopath="Enter the path of the video: "
-
+REM ----Downloads yt-dlp if not arleady installed----
+if not exist "%~dp0yt-dlp" (
+	echo downloading yt-dlp
+	curl -L "https://github.com/yt-dlp/yt-dlp/archive/refs/heads/master.zip" -o "%~dp0yt-dlp.zip"
+	tar -xf "%~dp0yt-dlp.zip" -C "%~dp0\"
+	move "%~dp0yt-dlp-*" "%~dp0yt-dlp"
+	del "%~dp0yt-dlp.zip"
+)
 
 REM ----checks if ffmpeg is in PATH----
 where /q ffmpeg
 if ERRORLEVEL 1 (
 	echo ffmpeg not found in PATH. using local install
-	if not exist ffmpeg\bin\ffmpeg.exe (
+	if not exist "%~dp0ffmpeg\bin\ffmpeg.exe" (
 		echo local install not found. downloading ffmpeg
 		REM ----downloads ffmpeg, extracts it, puts it into a file called \ffmpeg\, deletes the ffmpeg document and zip file, and deletes any ffmpeg namings left behind ----
-		curl -L -o %cd%\ffmpeg.zip "https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-lgpl.zip"
-		tar -xf %cd%\ffmpeg.zip -C %cd%
-		move %cd%\ffmpeg-* %~dp0\ffmpeg
-		rmdir /s /q %cd%\ffmpeg\doc
-		del %cd%\ffmpeg.zip 
-		for /d %%i in ("%cd%\ffmpeg-*") do (
+		curl -L "https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-lgpl-shared.zip" -o "%~dp0ffmpeg.zip"
+		tar -xf "%~dp0ffmpeg.zip" -C "%~dp0\"
+		move "%~dp0ffmpeg-*" "%~dp0ffmpeg"
+		rmdir /s /q "%~dp0ffmpeg\doc"
+		rmdir /s /q "%~dp0ffmpeg\include"
+		rmdir /s /q "%~dp0ffmpeg\lib"
+		del "%~dp0ffmpeg.zip"
+		for /d %%i in ("%~dp0ffmpeg-*") do (
 			if not "%%~nxi"=="ffmpeg" (
 				rmdir /s /q "%%i"
 			)
 		)
+		REM curl -L "https://github.com/BtbN/FFmpeg-Builds/releases/download/latest/ffmpeg-master-latest-win64-lgpl.zip" -o "%cd%\ffmpeg.zip"
+		REM tar -xf "%cd%\ffmpeg.zip" -C "%cd%"
+		REM move "%cd%\ffmpeg-*" "%~dp0\ffmpeg"
+		REM rmdir /s /q "%cd%\ffmpeg\doc"
+		REM del "%cd%\ffmpeg.zip"
+		REM for /d %%i in ("%cd%\ffmpeg-*") do (
+			REM if not "%%~nxi"=="ffmpeg" (
+				REM rmdir /s /q "%%i"
+			REM )
+		REM )
 	)
-	if "%choice%"=="1" (
-		call "%~dp0yt-dlp\yt-dlp.cmd" "%videopath%" --ffmpeg-location "%~dp0ffmpeg\bin\ffmpeg.exe" --paths "%cd%\Temp"
-	) else if "%choice%"=="2" (
-		call "%~dp0yt-dlp\yt-dlp.cmd" "%videopath%" --extract-audio --ffmpeg-location "%~dp0ffmpeg\bin\ffmpeg.exe" --paths "%cd%\Temp"
-	) else (
-		echo Invalid choice. Please enter 1 or 2.
-	)
+	set ffmpegLocation=--ffmpeg-location "%~dp0ffmpeg\bin\ffmpeg.exe"
 ) else (
-	if "%choice%"=="1" (
-		call "%~dp0yt-dlp\yt-dlp.cmd" "%videopath%" --paths "%cd%\Temp"
-	) else if "%choice%"=="2" (
-		call "%~dp0yt-dlp\yt-dlp.cmd" "%videopath%" --extract-audio --paths "%cd%\Temp"
-	) else (
-		echo Invalid choice. Please enter 1 or 2.
-	)	
+	set ffmpegLocation=
 )
-move %cd%\Temp\* %cd%
+set /a flag=1
+if "%videochoice%"=="1" (
+	set extractAudio=
+) else if "%videochoice%"=="2" (
+	set extractAudio=--extract-audio
+	REM Ask the user for the format of audio
+	echo What audio format should it be (ex:mp3^)(leave blank to be same as source^)
+	set /p audioFormat=">>"
+) else (
+	set /a flag=0
+	set videopath=%videochoice%
+)
+
+if defined audioFormat (
+	set audioFormat=--audio-format %audioFormat%
+)
+
+if %flag%==1 (
+	REM Ask the user for the path of the video
+	echo Enter the path of the video: 
+	set /p videopath=">>"
+	echo "%~dp0yt-dlp\yt-dlp.cmd" "%%videopath%%" %extractAudio% %ffmpegLocation% --paths "%~dp0Temp" %audioFormat%> "%~dp0currentCommand.txt"
+) else (
+	if not exist "%~dp0currentCommand.txt" (
+		echo couldent find last command. defaulting to video download.
+		echo "%~dp0yt-dlp\yt-dlp.cmd" "%%videopath%%" %ffmpegLocation% --paths "%~dp0Temp"> "%~dp0currentCommand.txt"
+	)
+)
+for /f "usebackq delims=" %%i in ("%~dp0currentCommand.txt") do (
+	REM @echo %%i
+	call %%i
+)
+rem call "%~dp0yt-dlp\yt-dlp.cmd" "%videopath%" %extractAudio% %ffmpegLocation% --paths "%~dp0Temp" --audio-format mp3
+move "%~dp0Temp\*" "%cd%"
 
 echo file(s) output to %cd%
 pause
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Super simple, super fast, Uses youtube-dl's python library. Just open, select if you want video or just audio, and paste the url.
+Super simple, super fast, Uses youtube-dl's python library. Just open, and paste the link to the video.
 
 To install:
 ----
@@ -8,16 +8,20 @@ To install:
 how to use:
 ----
 1.	run the batch file
-2.	type 1 for video or 2 for audio in the console
-3.  paste a youtube link into the console
-- the output will appear in the same directory as the batch file.
+2.	type 1 for video or 2 for audio or just paste a youtube link to use the most recent settings  
+If it's the first time running the script (or currentCommand.txt is missing) it will default to downloading video 
+3.  paste a youtube link into the console (if you chose 1)  
+if you chose 2 then you can enter the audio format (mp3, wav, ...) or leave blank to auto format to same as source
+- the output will appear in the same directory as the batch file (or wherever the main commandline is being run)
 
 Notes:
 ----
 - It checks if you have ffmpeg in your PATH. If not then it downloads it to a folder named ffmpeg in the batch directory
+- It will automatically download yt-dlp on first run.
 
 Todo:
 ----
 - [ ] add pass-through flags if using secondary batch file with the current values as default.
 - [ ] add more flags to be added if wanted through the console interface
+- [ ] add ability to download multiple videos at once.
 
